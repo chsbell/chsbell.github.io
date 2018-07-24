6a53261884ec760a83b51e36ba3c8c3a0db8109d
chsbell <yejt@chsbell.com>
2018-07-24 18:07:49 +0800

    update

---
+++ contactus.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Design:Albert ye
+ * User: chsbell
+ * Date: 2018/7/13/013
+ * Time: 17:17
+ */
+
+
+
+
+?>
+
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>深圳市华盛贝尔科技有限公司</title>
+
+</head>
+<link href="css/layout.css" rel="stylesheet" type="text/css" />
+
+<body>
+
+
+
+
+
+
+
+
+</body>
+<foot>
+<link href="webfoot.php">
+</foot>
+</html>
